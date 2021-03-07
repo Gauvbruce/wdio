886cd53baecc92ae04b56c20b3845e610a4f7308
Gauvbruce <Gauvbruce@gmail.com>
2021-03-07 20:22:52 +1300

    changes in my test.js

--- tests/mytest.js
+++ tests/mytest.js
@@ -1,16 +1,27 @@
 const { expect } = require('chai');
 const { default: pause } = require('webdriverio/build/commands/browser/pause');
+const loginPage = require('../pages/pageobjects/login.page');
 const LoginPage = require('../pages/pageobjects/login.page');
 const SecurePage = require('../pages/pageobjects/secure.page');
 
 describe('My second test', () => {
+
+    beforeEach(() => {
+        browser.maximizeWindow();
+        //browser.setWindowSize(1280, 870);
+      })
+
+
+
     it('should run this test', () => {
         LoginPage.open();
 
         LoginPage.login('gauvbruce112@gmail.com', 'Salesforce@123');
        // pause(10000);
        //assert.equal($(LoginPage.getTitle()), 'Lightning Experience', 'User is on the homepage');
-        expect(LoginPage.getTitle()).to.equal('Lightning Experience');
+       browser.setTimeout({ 'pageLoad': 10000 })
+       expect(LoginPage.getTitle()).to.equal('Lightning Experience');
+        //AllureReporter(loginPage.getTitle());
         //expect(SecurePage.flashAlert).toBeExisting();
        // expect(SecurePage.flashAlert).toHaveTextContaining(
         //    'You logged into a secure area!');
